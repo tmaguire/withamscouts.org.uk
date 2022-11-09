40029f44acfd5f60c89a461564b955a3388c8117
Tom Maguire <thomas@maguire.email>
2022-11-09 21:51:22 +0000

    Removed

--- js/src/treeview.js
+++
@@ -1,9 +0,0 @@
-var toggler = document.getElementsByClassName("rotate");
-var i;
-
-for (i = 0; i < toggler.length; i++) {
-  toggler[i].addEventListener("click", function() {
-    this.parentElement.querySelector(".nested").classList.toggle("active");
-    this.classList.toggle("down");
-  });
-}
